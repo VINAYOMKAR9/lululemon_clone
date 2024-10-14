6b9a982c00742d35f741245d28f6a61df4b34f24
VINAY OMKAR <109261762+VINAYOMKAR9@users.noreply.github.com>
2024-10-14 19:45:42 +0530

    Add files via upload

---
+++ backend/models/men.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const menSchema  = mongoose.Schema({
+image:{type:String,required:true},
+title:{type:String,required:true},
+price:{type:Number,required:true}
+})
+
+const MenModel = mongoose.model("men",menSchema)
+
+module.exports = {
+    MenModel
+}
